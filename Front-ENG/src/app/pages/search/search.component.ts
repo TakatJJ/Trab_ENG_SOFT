7a71a099b03e7fd1590df7e3dfbd002f26bb7279
Galeano Folle Domingues <galeanodomingues@gmail.com>
2024-07-09 09:09:41 -0300

    Changes to the search component.

--- Front-ENG/src/app/pages/search/search.component.ts
+++ Front-ENG/src/app/pages/search/search.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { BackAPIService } from '../../services/back-api.service';
 import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import {MatSliderModule} from '@angular/material/slider';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [ReactiveFormsModule,CommonModule],
+  imports: [ReactiveFormsModule,CommonModule, MatSliderModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
@@ -22,9 +23,12 @@ export class SearchComponent {
   
   filters = new FormGroup({
     campus: new FormControl('', [Validators.required]),
-    priceRange: new FormControl(1, [Validators.required, Validators.min(1)]),
+    minPrice: new FormControl(200, [Validators.required]),
+    maxPrice: new FormControl(2000, [Validators.required]),
     numberOfRoommates: new FormControl(0,),
     sameGender: new FormControl(false),
   });
-
+  onSearch() {
+    console.log(this.filters.value);
+  }
 }
