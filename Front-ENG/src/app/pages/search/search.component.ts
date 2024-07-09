423a3ddf204377a2594aeeca17f28170bb0ecb91
Galeano Folle Domingues <galeanodomingues@gmail.com>
2024-07-09 09:49:56 -0300

    Changes on button design and colors.

--- Front-ENG/src/app/pages/search/search.component.ts
+++ Front-ENG/src/app/pages/search/search.component.ts
@@ -1,33 +1,37 @@
 import { Component } from '@angular/core';
 import { BackAPIService } from '../../services/back-api.service';
-import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import {MatSliderModule} from '@angular/material/slider';
+import { MatSliderModule } from '@angular/material/slider';
+import { Campus } from '../../enums/Campus';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [ReactiveFormsModule,CommonModule, MatSliderModule],
+  imports: [ReactiveFormsModule, CommonModule, MatSliderModule],
   templateUrl: './search.component.html',
-  styleUrl: './search.component.css'
+  styleUrl: './search.component.css',
 })
 export class SearchComponent {
-  private API : BackAPIService;
-  constructor
-  (
-    APIService: BackAPIService,
-  ) 
-  {
+  private API: BackAPIService;
+  constructor(APIService: BackAPIService) {
     this.API = APIService;
   }
-  
+
   filters = new FormGroup({
     campus: new FormControl('', [Validators.required]),
     minPrice: new FormControl(200, [Validators.required]),
     maxPrice: new FormControl(2000, [Validators.required]),
-    numberOfRoommates: new FormControl(0,),
+    numberOfRoommates: new FormControl(0, [Validators.required]),
     sameGender: new FormControl(false),
   });
+
+  campusList = Campus;
   onSearch() {
     console.log(this.filters.value);
   }
