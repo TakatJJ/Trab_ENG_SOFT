e4a54f371888050ab09932c580fcc10076425b4a
Galeano Folle Domingues <galeanodomingues@gmail.com>
2024-08-09 11:06:01 -0300

    Update: Mudança em notificação.

--- Front-ENG/src/app/models/Notification.ts
+++ Front-ENG/src/app/models/Notification.ts
@@ -2,6 +2,7 @@ import { AdvertisementRESPONSE } from './Advertisement/AdvertisementRESPONSE';
 import { UserLogged } from './User/UserLogged';
 
 export default interface Notification {
+  id: number;
   LocatarioID: number;
   State: string;
   Room: AdvertisementRESPONSE;
